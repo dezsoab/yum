92acf0f464a44e3c8e02aeaca7fb6e524dcb4c87
Dezso Binder <binderdezso97@gmail.com>
2023-01-15 17:26:43 +0100

    closes #12 add client routing

--- yum-client/src/components/App.js
+++ yum-client/src/components/App.js
@@ -1,11 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
