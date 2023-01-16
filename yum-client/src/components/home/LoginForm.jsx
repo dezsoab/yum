839c845dbafd3b6d26147074aadca2d10f3d53c9
Dezso Binder <binderdezso97@gmail.com>
2023-01-16 16:11:21 +0100

    resolves #19 log in user with form

--- yum-client/src/components/home/LoginForm.jsx
+++ yum-client/src/components/home/LoginForm.jsx
@@ -1,5 +1,70 @@
+import React, { useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../store/UserContext";
+
 const LoginForm = () => {
-  return <h1>login form</h1>;
+  const username = useRef();
+  const password = useRef();
+  const navigate = useNavigate();
+
+  const { setToken } = useContext(UserContext);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: username.current.value,
+      password: password.current.value,
+    };
+
+    const res = await fetch("http://localhost:8080/api/v1/auth/authenticate", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+
+    if (res.status === 200) {
+      setToken(data);
+      navigate("/feed");
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <h2>Login</h2>
+      <p>Please fill in this form to log in to your account.</p>
+      <hr />
+
+      <label for="username">
+        <b>Username</b>
+      </label>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        name="username"
+        required
+        ref={username}
+      />
+
+      <label for="password">
+        <b>Password</b>
+      </label>
+      <input
+        type="password"
+        placeholder="Enter Password"
+        name="password"
+        id="password"
+        required
+        ref={password}
+      />
+
+      <button type="submit">Log in</button>
+    </form>
+  );
 };
 
 export default LoginForm;
