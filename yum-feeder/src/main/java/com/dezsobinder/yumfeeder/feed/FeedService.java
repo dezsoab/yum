59f16f1cc27b9435ac02f2dadb95ca197e272757
Dezso Binder <binderdezso97@gmail.com>
2023-01-20 22:01:51 +0100

    resolves #37 reconfigure motor values
    after re-building the feeder machine to an other mechanism, the motor had to be reconfigured since it does not open and close a door anymore, but rotates a 6 winged wheel for more accurate portion sizes.

--- yum-feeder/src/main/java/com/dezsobinder/yumfeeder/feed/FeedService.java
+++ yum-feeder/src/main/java/com/dezsobinder/yumfeeder/feed/FeedService.java
@@ -1,5 +1,8 @@
 package com.dezsobinder.yumfeeder.feed;
 
+import com.dezsobinder.yumfeeder.motor.ServoMotor;
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,24 +12,24 @@ public class FeedService {
     private int TIME_FOR_SERVO_RESET;
     @Value("${servo.pin}")
     private int PWM_PIN;
-    @Value("${servo.state.opened}")
-    private int MAX_OPEN_VALUE;
-    @Value("${servo.state.closed}")
-    private int MAX_CLOSE_VALUE;
+    @Value("${servo.state.start}")
+    private int SERVO_START_VALUE;
+    @Value("${servo.state.stop}")
+    private int SERVO_STOP_VALUE;
     @Value("${portion.default.size}")
     private int DEFAULT_PORTION_SIZE;
 
     public FeedResponse feed(Double requestedPortion) throws InterruptedException {
         double portionToFeed = DEFAULT_PORTION_SIZE * requestedPortion; // eg.: 1000ms * 2.2s = 2200ms
-//
-//        Context context = Pi4J.newAutoContext();
-//        ServoMotor servoMotor = new ServoMotor(context, PWM_PIN);
-//        servoMotor.setPercent(MAX_OPEN_VALUE);
-//        Thread.sleep((long) portionToFeed);
-//        servoMotor.setPercent(MAX_CLOSE_VALUE);
-//        Thread.sleep(TIME_FOR_SERVO_RESET);
-//        servoMotor.off();
-//        context.shutdown();
+
+        Context context = Pi4J.newAutoContext();
+        ServoMotor servoMotor = new ServoMotor(context, PWM_PIN);
+        servoMotor.setPercent(SERVO_START_VALUE);
+        Thread.sleep((long) portionToFeed);
+        servoMotor.setPercent(SERVO_STOP_VALUE);
+        Thread.sleep(TIME_FOR_SERVO_RESET);
+        servoMotor.off();
+        context.shutdown();
 
         return FeedResponse.builder()
                 .message("Pet has been fed")
